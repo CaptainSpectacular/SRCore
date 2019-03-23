687078d619f29f227d94b2b7b39adf4baef96a40
cpollock <pollock.clee@gmail.com>
2019-03-23 11:22:47 -0600

    Add project files.

---
+++ StarRealmsCore/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace StarRealmsCore.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
