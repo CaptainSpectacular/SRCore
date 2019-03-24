215cf1442fae4c67fb32dae2f2b7527fa8c7d729
cpollock <pollock.clee@gmail.com>
2019-03-24 14:40:21 -0600

    configured testing w/ xUnit

---
+++ StarRealmsCoreTests/Features/BasicIntegration.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using Microsoft.AspNetCore.Mvc.Testing;
+using System.Threading.Tasks;
+
+namespace StarRealmsCoreTests.Features
+{
+    public class BasicIntegration
+        : IClassFixture<WebApplicationFactory<StarRealmsCore.Startup>>
+    {
+        private readonly WebApplicationFactory<StarRealmsCore.Startup> _factory;
+        public BasicIntegration(WebApplicationFactory<StarRealmsCore.Startup> factory)
+        {
+            _factory = factory;
+        }
+        
+        [Theory]
+        [InlineData("/")]
+        public async Task TestHomePage(string url)
+        {
+            var client = _factory.CreateClient();
+
+            var response = await client.GetAsync(url);
+            response.EnsureSuccessStatusCode();
+            Assert.Equal("text/html; charset=utf-8", response.Content.Headers.ContentType.ToString());
+        }
+    }
+}
