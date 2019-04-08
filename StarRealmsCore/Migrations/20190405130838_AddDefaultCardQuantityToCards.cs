198fae3ba1c9f0e30a818639e2faa5a6a37ddb67
pollockcl <pollock.clee@gmail.com>
2019-04-07 19:34:41 -0600

    added default quantity to the card

---
+++ StarRealmsCore/Migrations/20190405130838_AddDefaultCardQuantityToCards.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace StarRealmsCore.Migrations
+{
+    public partial class AddDefaultCardQuantityToCards : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "DefaultQuantity",
+                table: "Cards",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DefaultQuantity",
+                table: "Cards");
+        }
+    }
+}
