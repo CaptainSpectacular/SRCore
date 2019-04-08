93209b5d307ce33385fde06f8fa73e354b4b5125
pollockcl <pollock.clee@gmail.com>
2019-04-07 19:35:27 -0600

    slightly changed test, also added test for populating a games main deck

--- StarRealmsCoreTests/ServiceTests/DeckServiceTests.cs
+++ StarRealmsCoreTests/ServiceTests/DeckServiceTests.cs
@@ -41,30 +41,77 @@ namespace StarRealmsCoreTests.ServiceTests
                     PlayerId = 1,
                     Type = 1
                 };
-                Card corvette = new Card
+                Card scout = new Card
                 {
-                    Name = "Corvette",
-                    Cost = 0
+                    Name = "Scout",
+                    Cost = 0,
+                    DefaultQuantity = 8
                 };
                 Card viper = new Card
                 {
                     Name = "Viper",
-                    Cost = 0
+                    Cost = 0,
+                    DefaultQuantity = 2
                 };
+                context.Cards.Add(scout);
                 context.Cards.Add(viper);
-                context.Cards.Add(corvette);
 
                 service.CreateDeck(deck);
 
                 var d = service.GetDeck(1);
                 int totalQuantity = d.Cards.Select(x => x.Quantity).Sum();
-                int cQuantity = d.Cards.First().Quantity;
-                int vQuantity = d.Cards.Last().Quantity;;
+                int scoutQuantity = d.Cards.First().Quantity;
+                int viperQuantity = d.Cards.Last().Quantity;;
                 
                 Assert.Equal(2, d.Cards.Count());
                 Assert.Equal(10, totalQuantity);
-                Assert.Equal(8, cQuantity);
-                Assert.Equal(2, vQuantity);
+                Assert.Equal(8, scoutQuantity);
+                Assert.Equal(2, viperQuantity);
+            }
+        }
+
+        [Fact]
+        public void TestCreatingMainDeck()
+        {
+            using (var context = new AppDbContext(CreateNewContextOptions()))
+            {
+                for (int i = 0; i < 20; i++)
+                {
+                    Card card = new Card
+                    {
+                        Name = "DummyCard" + i.ToString(),
+                        Cost = i,
+                        DefaultQuantity = 10 
+                    };
+
+                    context.Cards.Add(card);
+                    context.SaveChanges();
+                }
+                Player player1 = new Player { Name = "ZTO" };
+                Player player2 = new Player { Name = "CS" };
+                context.Players.Add(player1);
+                context.Players.Add(player2);
+                context.SaveChanges();
+                Game game = new Game
+                {
+                    PlayerTurn = 0
+                };
+                context.SaveChanges();
+                PlayerGame pg1 = new PlayerGame { PlayerId = 1, GameId = 1 };
+                PlayerGame pg2 = new PlayerGame { PlayerId = 2, GameId = 1 };
+                context.PlayerGames.Add(pg1);
+                context.PlayerGames.Add(pg2);
+                context.SaveChanges();
+                DeckService deckService = new DeckService(context);
+                deckService.CreateMainDeck(game.Id);
+                var deck = deckService.GetDeck(1);
+
+                int totalCount = deck.Cards.Select(x => x.Quantity).Sum();
+
+
+                // should not add cards with a cost of 0
+                Assert.Equal(19, deck.Cards.Count());
+                Assert.Equal(190, totalCount);
             }
         }
     }
